e8caaaf36c8d16e939bd8665e1ced1d7e5a68026
Paper Girl <jamie.rose.528@gmail.com>
2021-11-23 01:14:01 -0800

    added time blocks and color coding

--- Assets/Moment.js
+++ Assets/Moment.js
@@ -1,3 +1,26 @@
 var currentDay = $("#currentDay")
 
-currentDay.textContent = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+// Timer
+var now = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+console.log(currentDay)
+
+currentDay.text(now)
+setInterval(() => {
+    now = moment().format("dddd, MMMM Do YYYY, h:m:ss a")
+    currentDay.text(now);
+}, 1000);
+
+// each timeblock is color coded to indicate whether it is in the past, present, or future
+var timeBlock = function () {
+    $(".row").each(function() {
+        var time = $(this).attr("data-time").replace("time", "");
+        if (time < currentDay) {
+            $(this).children(".description").addClass("past");
+        } else if (time > currentDay) {
+            $(this).children(".description").addClass("future");
+        } else {
+            $(this).children(".desciption").addClass("present")
+        }
+    })
+}
+timeBlock();
